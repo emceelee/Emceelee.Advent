2e17e05dba8311e49c4720e0f8d11260a28ace2d
Matthew Lee <matthew_lee@qbsol.com>
2021-12-16 14:49:58 -0600

    Day 16 - Prime Color Image Decoding

---
+++ Emceelee.Advent.Forms/PrimeResolver.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emceelee.Advent.Forms
+{
+    public class PrimeResolver
+    {
+        //populate with primes as we increment up to n
+        private HashSet<int> Primes { get; } = new HashSet<int>();
+
+        public HashSet<int> ResolvePrimes(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Number must be > 0");
+            }
+
+            var result = new HashSet<int>();
+            var possiblePrimes = Enumerable.Range(1, n);
+
+            foreach (var possiblePrime in possiblePrimes)
+            {
+                if (IsPrime(possiblePrime))
+                {
+                    if (!Primes.Contains(possiblePrime))
+                    {
+                        Primes.Add(possiblePrime);
+                    }
+                    if (!result.Contains(possiblePrime))
+                    {
+                        result.Add(possiblePrime);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        //must execute numbers less than n before n
+        private bool IsPrime(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return false;
+            }
+
+            foreach (var prime in Primes.Where(i => i <= Math.Ceiling(Math.Sqrt(n))))
+            {
+                if (prime >= n)
+                {
+                    break;
+                }
+                if (n % prime == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Emceelee.Advent.Forms/Solution_16.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Emceelee.Advent.Forms
+{
+    public partial class Solution_16 : Form
+    {
+        private enum RGB
+        {
+            R,
+            G,
+            B
+        }
+
+        private HashSet<int> Primes { get; set; }
+
+        public Solution_16()
+        {
+            InitializeComponent();
+
+            Primes = new PrimeResolver().ResolvePrimes(255);
+        }
+
+        private void Solution_16_Paint(object sender, PaintEventArgs e)
+        {
+            Bitmap image = new Bitmap("16_coded.png");
+            var resolver = new PrimeResolver();
+
+            e.Graphics.DrawImage(image, 0, 0, image.Width, image.Height);
+
+            for (int x = 0; x < image.Width; ++x)
+            {
+                for (int y = 0; y < image.Height; ++y)
+                {
+                    var color = image.GetPixel(x, y);
+                    //Set pixel to black if one of RGB is prime
+                    color = IsColorPrime(color, RGB.G) ? Color.Black : Color.White;
+
+                    image.SetPixel(x, y, color);
+                }
+            }
+
+            e.Graphics.DrawImage(image, 0, image.Height+1, image.Width, image.Height);
+        }
+
+        private bool IsPrime(int n)
+        {
+            return Primes.Contains(n);
+        }
+
+        private bool IsColorPrime(Color c, RGB rgb)
+        {
+            bool result = false;
+            switch(rgb)
+            {
+                case RGB.R:
+                    result = IsPrime(c.R);
+                    break;
+                case RGB.G:
+                    result = IsPrime(c.G);
+                    break;
+                case RGB.B:
+                    result = IsPrime(c.B);
+                    break;
+            }
+            return  result;
+        }
+    }
+}
